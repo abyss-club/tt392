ec6121fbcd83c0816d330484dd40f973d0c0d3f2
nanozuki <nanozuki.crows@gmail.com>
2018-07-02 22:26:55 +0800

    update readme, fixed some error

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const Wrapper = styled.div`
 
 const IndexWrapper = styled.div`
   margin: 0 16px;
-  @media screen (min-width: 992px) {
-    max-width: 1000px;
+  @media (min-width: 992px) {
+    max-width: 960px;
     margin: auto;
   }
 `;
@@ -24,14 +24,17 @@ const Index = () => (
     <p>本站尚处于开发初期</p>
     <p>后端由 <a href="https://gitlab.com/abyss.club/uexky">uexky</a> 项目驱动</p>
     <p>前端由 <a href="https://gitlab.com/abyss.club/tt392">tt392</a> 项目驱动</p>
-    <br/>
+    <br />
     <p>目前尚无法通过交互式界面使用 abyss，所幸我们使用的是 GraphQL API
       ( <a href="https://graphql.org/">了解 GraphQL</a> )，
-    你可以在下面的链接中，使用 GraphiQL 工具，交互地通过 API 使用本站。</p>
+    你可以点击下面的链接，使用 GraphiQL 工具，交互地通过 API 使用本站。
+    </p>
     <Link to="/graphiql/"> GraphiQL </Link>
-    <p>Use GraphQL Voyager to inspect the schema.</p>
+    <p>你可以点击下面的链接，使用 GraphQL Voyager 探索 API schema。</p>
     <Link to="/voyager/"> GraphQL Voyager </Link>
-    <p>GraphQL API 本身具有自省的功能，但如果你想看的话，schema <a href="">在此</a></p>
+    <p>GraphQL API 本身有自省的功能，但如果你想看的话，
+      <a href="https://gitlab.com/abyss.club/abyss/blob/master/api.gql">schema 在此</a>
+    </p>
   </IndexWrapper>
 );
 
