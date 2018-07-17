524480c1cc0a76a87e1331b26de81816fff76ec2
OrcaXS Suen <sun16281394@gmail.com>
2018-07-17 22:54:25 +0800

    Add colors.js

---
+++ src/utils/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  orange: '#E85031',
+  'orange-light': '#F95C3C',
+  vulcan: '#363A3E',
+  'vulcan-light': '#43484C',
+  aluminium: '#818890',
+  'aluminium-light': '#979FA7',
+  zircon: '#EBEDEE',
+  'zircon-light': '#FAFBFC',
+  skyblue: '#12AAFF',
+  indigo: '#7D12FF',
+};
+
+export default colors;
