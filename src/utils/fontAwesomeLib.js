9a753d92c5bed086b407ff68da1bde7fcfb2565a
OrcaXS Suen <sun16281394@gmail.com>
2018-08-04 19:48:43 +0800

    Add fa Icons

--- src/utils/fontAwesomeLib.js
+++ src/utils/fontAwesomeLib.js
@@ -9,12 +9,14 @@ import {
   faUser,
   faPlusSquare,
   faBars,
+  faChevronDown,
+  faChevronUp,
 } from '@fortawesome/free-solid-svg-icons';
 
 const faLib = {
   loadFa: () => {
     // eslint-disable-next-line max-len
-    library.add(faCheckSquare, faCoffee, faLink, faImage, faTimes, faSpinner, faUser, faPlusSquare, faBars);
+    library.add(faCheckSquare, faCoffee, faLink, faImage, faTimes, faSpinner, faUser, faPlusSquare, faBars, faChevronDown, faChevronUp);
   },
 };
 
