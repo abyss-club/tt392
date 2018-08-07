667edd4da9f9986ae3833d685f34454383271b8d
nanozuki <nanozuki.crows@gmail.com>
2018-08-07 14:32:45 +0800

    start add context

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
 import { ApolloProvider } from 'react-apollo';
 
 import App from 'App';
-import Modal from 'components/Modal';
+import client from 'providers/client';
+import Feedback from 'providers/Feedback';
+import Store from 'providers/Store';
 import registerServiceWorker from 'registerServiceWorker';
 
 import 'normalize.css';
@@ -18,33 +14,16 @@ import 'assets/css/fonts.css';
 
 import './index.css';
 
-const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors) {
-        graphQLErrors.map(({ message, locations, path }) =>
-          console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`));
-      }
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    new HttpLink({
-      uri: 'http://api.uexky.com/graphql/',
-      credentials: 'include',
-      fetchOptions: {
-        mode: 'cors',
-      },
-    }),
-  ]),
-  cache: new InMemoryCache(),
-});
 
 const Root = () => (
   <BrowserRouter>
-    <Modal.Provider>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-    </Modal.Provider>
+    <Store.Provider>
+      <Feedback.Provider>
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
+      </Feedback.Provider>
+    </Store.Provider>
   </BrowserRouter>
 );
 
