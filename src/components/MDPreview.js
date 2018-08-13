a7d5f5bb5c33a6b6167309b833ff3e8a621098cb
OrcaXS Suen <sun16281394@gmail.com>
2018-08-13 17:11:57 +0800

    Add max-width: 100% to images

--- src/components/MDPreview.js
+++ src/components/MDPreview.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
+import styled from 'styled-components';
 
+const StyledImg = styled.img`
+  max-width: 100%;
+`;
 
 const MDPreview = ({ text }) => (
-  <ReactMarkdown source={text} />
+  <ReactMarkdown source={text} renderers={{ image: Image, imageReference: Image }} />
 );
 MDPreview.propTypes = {
   text: PropTypes.string.isRequired,
+};
+
+function Image(props) {
+  return <StyledImg alt={props.alt} src={props.src} />;
+}
+Image.propTypes = {
+  alt: PropTypes.string,
+  src: PropTypes.string.isRequired,
+};
+Image.defaultProps = {
+  alt: 'Description',
 };
 
 export default MDPreview;
