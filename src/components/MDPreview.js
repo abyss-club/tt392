ddffcaa9e5ee007b6d76f0988419d502f1718481
OrcaXS Suen <sun16281394@gmail.com>
2018-08-13 18:58:39 +0800

    Use nested styles instead

--- src/components/MDPreview.js
+++ src/components/MDPreview.js
@@ -3,26 +3,19 @@ import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
 import styled from 'styled-components';
 
-const StyledImg = styled.img`
-  max-width: 100%;
+const PreviewWrapper = styled.div`
+  img {
+    max-width: 100%;
+  }
 `;
 
 const MDPreview = ({ text }) => (
-  <ReactMarkdown source={text} renderers={{ image: Image, imageReference: Image }} />
+  <PreviewWrapper>
+    <ReactMarkdown source={text} />
+  </PreviewWrapper>
 );
 MDPreview.propTypes = {
   text: PropTypes.string.isRequired,
-};
-
-function Image(props) {
-  return <StyledImg alt={props.alt} src={props.src} />;
-}
-Image.propTypes = {
-  alt: PropTypes.string,
-  src: PropTypes.string.isRequired,
-};
-Image.defaultProps = {
-  alt: 'Description',
 };
 
 export default MDPreview;
