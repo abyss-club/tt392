042c562553752800f2a14837cf04e6e957f8504c
OrcaXS Suen <sun16281394@gmail.com>
2018-09-08 00:21:07 +0800

    Adjust font-sizes of h1-6 and p, remove margins

--- src/components/MDPreview.js
+++ src/components/MDPreview.js
@@ -4,15 +4,28 @@ import ReactMarkdown from 'react-markdown';
 import styled from 'styled-components';
 
 const PreviewWrapper = styled.div`
+  * {
+    font-size: .875rem;
+    color: ${props => (props.isThread ? colors.textGrey : colors.regularBlack)};
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: bold;
+    margin: 0;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  h2, h3, h4, h5, h6 {
+    font-size: .875rem;
+  }
+
   h1 {
-    font-size: 1.25rem;
-  }
-  h2 {
-    font-size: 1.125rem;
-  }
-  h3, h4, h5 {
     font-size: 1rem;
   }
+
   img {
     max-width: 100%;
   }
