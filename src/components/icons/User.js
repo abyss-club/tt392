626b73b16d67b61b0bd5b9fd62bea250a62394d3
OrcaXS Suen <sun16281394@gmail.com>
2018-09-08 21:36:39 +0800

    Add new icons and their components

---
+++ src/components/icons/User.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const User = props => (
+  <svg viewBox="0 0 24 24" fill="none" width="1em" height="1em" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M20 16.912v2.677c0 .227-.224.411-.5.411h-15c-.276 0-.5-.184-.5-.411v-2.677C4 14.752 7.582 13 12 13s8 1.752 8 3.912zM16 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0z"
+      fill="#fff"
+    />
+  </svg>
+);
+
+export default User;
