3aec1cfcbdbc40739e64d7491fcc093e78db13e0
OrcaXS Suen <sun16281394@gmail.com>
2018-09-10 17:51:57 +0800

    Add new icons

---
+++ src/components/icons/AngleDown.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AngleDown = props => (
+  <svg viewBox="0 0 12 12" fill="none" width="1em" height="1em" {...props}>
+    <path
+      d="M2 4l3.823 3.922a.24.24 0 0 0 .354 0L10 4"
+      stroke="#1A1A1A"
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
+export default AngleDown;
