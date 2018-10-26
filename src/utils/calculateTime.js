589973a8ef708cb4c3633a9806979048d4615c13
OrcaXS Suen <sun16281394@gmail.com>
2018-10-26 13:07:16 +0800

    Optimize time format

--- src/utils/calculateTime.js
+++ src/utils/calculateTime.js
@@ -1,6 +1,7 @@
 function timeElapsed(origTime) {
   const now = Date.now();
-  const elapsed = now - new Date(origTime);
+  const origDate = new Date(origTime);
+  const elapsed = now - origDate;
   let formatted = '';
   if (elapsed < 60000) {
     formatted = `${new Date(elapsed).getUTCSeconds()}s`;
@@ -8,8 +9,13 @@ function timeElapsed(origTime) {
     formatted = `${new Date(elapsed).getUTCMinutes()}m`;
   } else if (elapsed < 86400000) {
     formatted = `${new Date(elapsed).getUTCHours()}h${new Date(elapsed).getUTCMinutes()}m`;
-  } else {
+  } else if (elapsed < 259200000) {
     formatted = `${new Date(elapsed).getUTCDate()}d${new Date(elapsed).getUTCHours()}h`;
+  } else {
+    const options = {
+      year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false,
+    };
+    formatted = origDate.toLocaleString('en-CA', options).replace(',', '');
   }
   return {
     day: new Date(elapsed).getUTCDate(),
